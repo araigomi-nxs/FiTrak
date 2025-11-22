e911acbe0d8e0c8d5eac5913ed250e2dc96aa764
nexuswolfxd <98227138+nexuswolfxd@users.noreply.github.com>
2025-11-22 21:20:17 +0800

    ADDED datetime picker and live stopwatch function to calculate duration

--- src/main/java/layouts/calculator/basic/RunningWorkoutCalculator.java
+++ src/main/java/layouts/calculator/basic/RunningWorkoutCalculator.java
@@ -1,11 +1,17 @@
 package layouts.calculator.basic;
 
 import calculationModels.basic.RunningWorkout;
+import layouts.calculator.Stopwatch;
 import objects.Account;
+import raven.datetime.TimePicker;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 
 public class RunningWorkoutCalculator {
 
@@ -24,22 +30,31 @@ public class RunningWorkoutCalculator {
 
     private JPanel OutputPanel;
     private JTextArea outputTextArea;
+    private JPanel stopWatchArea;
+    private TimePicker timePicker;
+    private TimePicker timePicker2;
+    private JFormattedTextField StartTimeField;
+    private JFormattedTextField EndTimeField;
 
+    JLabel stopwatchButton;
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("hh:mm a");
 
     public RunningWorkoutCalculator(Account  account) {
 
-        //setContentPane(JPanel2);
-       //setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //pack();
-       // setSize(300, 400);
-       // setLocationRelativeTo(null);
-       // setVisible(true);
+        CardLayout cardLayout = new CardLayout();
+        stopWatchArea.setLayout(cardLayout);
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatchButton = stopwatch.getStartStopButton();
 
+        stopWatchArea.add(stopwatch.getPanel(), "stopwatch" );
+        cardLayout.show(stopWatchArea, "stopwatch");
 
-
-
-
-
+        timePicker = new TimePicker();
+        timePicker.setColor(new  Color(220, 228, 55));
+        timePicker.setEditor(StartTimeField); // link popup to field
+        timePicker2 = new TimePicker();
+        timePicker2.setColor(new  Color(220, 228, 55));
+        timePicker2.setEditor(EndTimeField);
 
         CalculateButton.addActionListener(e -> {
             try {
@@ -79,7 +94,25 @@ public class RunningWorkoutCalculator {
             }
         });
 
+        stopwatchButton.addMouseListener(new MouseAdapter() {
+
+            @Override
+            public void mouseClicked(MouseEvent e) {
+
+                DurationField.setText(stopwatch.getDurationMinutes());
+
+
+                LocalDateTime startDT = stopwatch.getStartDT();
+                timePicker.setSelectedTime(startDT.toLocalTime());
+                StartTimeField.setValue(startDT.toLocalTime().format(formatter));
+
+                LocalDateTime endDT = stopwatch.getEndDT();
+                timePicker2.setSelectedTime(startDT.toLocalTime());
+                EndTimeField.setValue(endDT.toLocalTime().format(formatter));
+            }
+        });
     }
+
     public void clearFields()
     {
         DurationField.setText("");
