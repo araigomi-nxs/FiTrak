d2ea1082fb731f543949c118a7fe01d303809681
nexuswolfxd <98227138+nexuswolfxd@users.noreply.github.com>
2025-11-23 10:13:51 +0800

    ADDED daily weight loss calculations for MetricsCalculator Class

--- src/main/java/calculationModels/metrics/MetricsCalculator.java
+++ src/main/java/calculationModels/metrics/MetricsCalculator.java
@@ -43,4 +43,54 @@ public class MetricsCalculator {
         else if (bmi < 30) return "Challenging – increase cardio";
         else return "Heavy – focus on weight management and low impact";
     }
+
+
+    //Basal Metabolic Rate
+    public static double computeBMR ( double weight, double height , int age, String sex ) {
+        double BMR;
+
+        BMR  =(10 * weight) + (6.25 * (height * 100)) -( 5 * age );
+        BMR = sex.equals("male")? BMR+5:BMR-161;
+        return BMR;
+    }
+
+    // Total Daily Energy Expenditure (TDEE)
+    public static double computeTDEE(double BMR, double dailyExerciseCalBurn, int preference) {
+        double activityFactor;
+        switch (preference) {
+              case 2: // Moderately Active
+                activityFactor = 1.55;
+                break;
+            case 3: // Very Active
+                activityFactor = 1.725;
+                break;
+            default: // Sedentary
+                activityFactor = 1.2;
+        }
+        double TDEE = BMR * activityFactor;
+
+        if (dailyExerciseCalBurn > 0) {
+            TDEE += dailyExerciseCalBurn;
+        }
+        return TDEE;
+    }
+
+
+    //Daily Cal Deficit assume calIntake for now edit later
+    public static double computeDailyCalDeficit(double TDEE, double calConsumed) {
+        return  TDEE - calConsumed;
+    }
+
+    //Daily FatLoss in grams
+    public static double computeFatLoss(double dailyCalDeficit)
+    {
+        //1 kilogram of fat ≈ 7,700 kcal average
+        double fatLoss;
+        fatLoss = dailyCalDeficit / 7700.0;
+        return fatLoss;
+    }
+
+
+    //wag muna display ang weightloss per day - calculator/update ng user. separate rin ang weigh in per week
+
 }
