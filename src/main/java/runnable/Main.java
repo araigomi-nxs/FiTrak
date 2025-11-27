75873ed4919da446debaeabccd2316f4457bb81f
nexuswolfxd <98227138+nexuswolfxd@users.noreply.github.com>
2025-11-28 05:22:18 +0800

    FINISHED - my final commit program defended

--- src/main/java/runnable/Main.java
+++ src/main/java/runnable/Main.java
@@ -11,7 +11,7 @@ import java.awt.*;
 import java.io.IOException;
 import java.net.InetAddress;
 
-
+//CREATED BY https://github.com/araigomi-nxs & https://github.com/llonaheinrich55-cyber
 public class Main {
     public static void main(String[] args) throws Exception {
 
